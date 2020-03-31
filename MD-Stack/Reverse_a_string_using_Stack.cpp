1df12f645322ebe192e211d4f2f860d7defad44a
Tharun Ravuri <41537996+tharun143@users.noreply.github.com>
2020-03-31 16:38:13 +0530

    Update Reverse_a_string_using_Stack.cpp

--- MD-Stack/Reverse_a_string_using_Stack.cpp
+++ MD-Stack/Reverse_a_string_using_Stack.cpp
@@ -1,15 +1,30 @@
+void reverse(char *str, int len);
+int main()
+{
+    long long int t;
+    cin>>t;
+    while(t--)
+    {
+        char str[10000];
+        cin>>str;
+        long long int len=strlen(str);
+        reverse(str,len);
+        cout<<str;
+        cout<<endl;
+    }
+        return 0;
+}
+// } Driver Code Ends
 void reverse(char *str, int len)
 {
-  stack <char> mystack;
-  for(int i=0;i<len;i++)
-  {
-    mystack.push(str[i]);
-  }
-  for(int i=0;i<len;i++)
-  {
-    str[i]=mystack.top();
-    mystack.pop();
-  }
+    stack <char> newstack;
+    for(int i=0;i<len;i++)
+    {
+        newstack.push(str[i]);
+    }
+    for(int i=0;i<len;i++)
+    {
+        str[i] = newstack.top();
+        newstack.pop();
+    }
 }
-
-// this function is used for reversing a string using stack
